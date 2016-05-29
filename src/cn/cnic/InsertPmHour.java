8ad3ff92573af94ea840042ff97f5f4d171e3ac4
gozky <530806081@qq.com>
2016-05-29 21:01:47 +0800

    将空气指标数据全为空的过滤掉（大约3%-4%）；对于个别数据项为空的赋值为-1；

--- src/cn/cnic/InsertPmHour.java
+++ src/cn/cnic/InsertPmHour.java
@@ -30,6 +30,7 @@ public class InsertPmHour {
         MongoCollection<Document> col = database.getCollection("pm_data"); //修改collection
         Document doc = new Document();
         int count = 0;
+        int null_cnt = 0;
 
         //System.out.println(tempList.length); 217个文件
         for(int i=0;i<tempList.length;i++){
@@ -50,6 +51,15 @@ public class InsertPmHour {
                             continue;
                         }else{
 
+                            //若前面几项空气指标都为空，那么该条数据删除
+                            if(data[8].equals("") && data[8].equals(data[13]) && data[13].equals(data[11])
+                                    &&data[11].equals(data[15]) && data[15].equals(data[19]) && data[19].equals(data[21])
+                                    && data[21].equals(data[9])){
+                                null_cnt++;
+                                //System.out.println("all is null");
+                                continue;
+                            }
+
                             // 为防止前面某个数据项为空，影响后面数据的插入，所以分开
                             try {
                                 doc.put("aqi",Float.parseFloat(data[8]));
@@ -57,36 +67,37 @@ public class InsertPmHour {
                                 //System.out.println("aqi: "+data[8]+","+"co "+data[13]+","+"no2 "+data[11]+","+"o3 "+data[15]+","+
                                 //    "pm10 "+data[19]+","+"pm25 "+data[21]+","+"so2 "+data[9]);
                                 //e.printStackTrace();
+                                doc.put("aqi",-1);
                             }
                             try {
                                 doc.put("co",Float.parseFloat(data[13]));
                             }catch (Exception e){
-
+                                doc.put("co",-1);
                             }
                             try {
                                 doc.put("no2",Float.parseFloat(data[11]));
                             }catch (Exception e){
-
+                                doc.put("no2",-1);
                             }
                             try {
                                 doc.put("o3",Float.parseFloat(data[15]));
                             }catch (Exception e){
-
+                                doc.put("o3",-1);
                             }
                             try {
                                 doc.put("pm10",Float.parseFloat(data[19]));
                             }catch (Exception e){
-
+                                doc.put("pm10",-1);
                             }
                             try {
-                                doc.put("pm25",Float.parseFloat(data[21]));;
+                                doc.put("pm25",Float.parseFloat(data[21]));
                             }catch (Exception e){
-
+                                doc.put("pm25",-1);
                             }
                             try {
                                 doc.put("so2",Float.parseFloat(data[9]));
                             }catch (Exception e){
-
+                                doc.put("so2",-1);
                             }
 
 
@@ -112,6 +123,8 @@ public class InsertPmHour {
 
 
                     }
+                    System.out.println("all null: "+null_cnt);
+                    System.out.println("count: "+count);
 
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
@@ -123,8 +136,8 @@ public class InsertPmHour {
                 System.out.println("文件不存在！");
             }
         }
-
-        System.out.println("count: "+count);
+        System.out.println("total all null: "+null_cnt);
+        System.out.println("total count: "+count);
     }
 
 
